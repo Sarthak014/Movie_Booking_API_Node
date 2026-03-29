4d4495f88e295f606b2e419cc01d561c7cb925a4
Sarthak Agarwal <sarthakadm@gmail.com>
2026-03-29 17:21:20 +0530

    feat: add movie routes in index.js

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ import mongoose from 'mongoose';
 
 import 'dotenv/config';
 
+import { movies } from './server/constants/routes.constant.js';
+import movieRoutes from './server/routes/movie.route.js';
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Routes
+app.use(movies, movieRoutes);
+
 /** MONGOOSE SETUP */
 const PORT = process.env.PORT || 3000;
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 mongoose
   .connect(process.env.DB_URL_LOCAL, {
